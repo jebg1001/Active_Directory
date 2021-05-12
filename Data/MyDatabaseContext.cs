2d2131f5223b708679c1b82840fdaf021030da5e
Juan Bejarano <jebg_10@hotmail.com>
2021-05-12 02:44:45 -0500

    Avance Ejemplo Lab Profesor

---
+++ Data/MyDatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Active_Directory.Models
+{
+    public class MyDatabaseContext : DbContext
+    {
+        public MyDatabaseContext (DbContextOptions<MyDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Active_Directory.Models.Alumno> Alumnos { get; set; }
+    }
+}
---
+++ Models/Alumno.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Active_Directory.Models
+{
+    public class Alumno
+    {
+        public int ID { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime Hora { get; set; }
+        public double Var1 { get; set; }
+        public double Var2 { get; set; }
+    }
+}
